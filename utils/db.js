590db8f04d1760803d6a588d12801f3c80f418f3
Supakit <book0970403815@hotmail.com>
2024-03-10 15:05:51 +0700

    first commit

---
+++ utils/db.js
@@ -0,0 +1,7 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = "mongodb://127.0.0.1:27017";
+
+export const client = new MongoClient(connectionString);
+
+export const db = client.db("practice-mongo");
